bde4d7d58694023095ca494de92ce8fb2c556b5c
arthur <arthurtibame@gmail.com>
2020-05-21 12:52:51 +0800

    add readme.md & data

--- price_api/price_api.py
+++ price_api/price_api.py
@@ -32,51 +32,51 @@ def requests_post(*args1, **args2):
         i -= 1
     return pd.DataFrame()
 
-def crawler(location):
+def crawler():
     #con = sqlite3.connect('D:\\python\\Chatbot\\GoBot\\shopAddress\\shop_details.db')
-    con = sqlite3.connect('./data/data.db')
+    #con = sqlite3.connect('./data/data.db')
     config = configparser.ConfigParser()
     config.read(r'./price_api/price.ini')
-        
+    locations = config.sections() # list of locations        
     year = [i for i in range(100,110) if i!=104]
-    try:
-        for i in tqdm(range(len(year))):
-            url = config.get(str(location), str(year[i]))
-            res = requests.get(url).json()
-            # set the offset
-            offset = 0
-            # set the limit
-            limit=10000
-            # get total then request again to control the loop amount and loop amount = total/limit + 1
-            loop_amount = res['result']['total']//limit+1        
-            for j in range(loop_amount):
 
-                url1 = url + '&&limit=' + str(limit) + '&&offset=' + str(offset)
-                res = requests.get(url1).json()
-                # json to dataframe
-                all_records = res['result']['records'][:]
-                df = pd.DataFrame(all_records)  
-                df = df.drop(columns=['_id'])
-                df.insert(0,'年份', [year[i]]*len(df))
-                df.to_sql(str(location) ,con=con,index=False, if_exists='append')
-                #df.to_csv("{}_{}-{}.csv".format(location,str(year[i]),j),index=None)
-                offset+=10000
+    try:
+
+        for i in tqdm(range(len(locations))):
+            for k in tqdm(range(len(year))):
+                url = config.get(str(locations[i]), str(year[k]))
+                res = requests.get(url).json()
+                # set the offset
+                offset = 0
+                # set the limit
+                limit=10000
+                # get total then request again to control the loop amount and loop amount = total/limit + 1
+                loop_amount = res['result']['total']//limit+1        
+                for j in range(loop_amount):
+
+                    url1 = url + '&&limit=' + str(limit) + '&&offset=' + str(offset)
+                    res = requests.get(url1).json()
+                    # json to dataframe
+                    all_records = res['result']['records'][:]
+                    df = pd.DataFrame(all_records)  
+                    df = df.drop(columns=['_id'])
+                    df.insert(0,'年份', [year[k]]*len(df))
+                    #df.to_sql(str(location) ,con=con,index=False, if_exists='append')
+                    df.to_csv("{}.csv".format(locations[i]),index=None,mode='a', header=False)
+                    
+                    offset+=10000
     except :
-        print('something wrong in : {}, {}, {}'.format(location, year[i], offset ))
+        print('something wrong in : {}, {}, {}'.format(locations[i], year[j], offset ))
     return 'finish'
         
 
 def main():
-    config = configparser.ConfigParser()
-    config.read(r'./price_api/price.ini') 
-    locations = config.sections() # list of locations
+    pass
     
-    for i in tqdm(range(len(locations))):        
-        crawler(str(locations[i]))
             
 
 if __name__ == "__main__":
-    main()    
+    crawler()
 
 
 
